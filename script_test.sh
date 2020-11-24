0f8ca6c95e99304bb174a7d080010894856deb06
sharibox <sharib.ali@eng.ox.ac.uk>
2020-11-24 11:35:33 +0000

    updated for 2 class

--- script_test.sh
+++ script_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# @author: sharib
 
 BASE_FOLDER=`pwd`
 echo $BASE_FOLDER
@@ -7,10 +8,13 @@ ROOT_FOLDER=$BASE_FOLDER
 source activate myenv
 
 # TO run 3 class classification using CNN
-nClasses=3
+nClasses=2 # change to 2 if two class and 3 for 3 class classification
 dataType='noExclusion' 
+
 # noExclusion, withExclusion, balanced
 echo '----> You are using the data ' $dataType 'for testing...'
+
+
 
 if (( $nClasses == '3' ))
 then 
@@ -23,9 +27,33 @@ then
 fi 
 
 ##========================= 2 class category training ===============================================##
+classPairSrting='1,2'
+classA='1'
+classB='2'
 if (( $nClasses == 2 ))
 then 
 
-    echo 'Not implemented!'
+    classPairSrting='1,2'
+    classA='1'
+    classB='2'
+    python $BASE_FOLDER/test_2class.py --test_data $BASE_FOLDER/data/$dataType'_test_data_'$classA'_'$classB'.csv' --test_label $BASE_FOLDER/data/$dataType'_test_label_'$classA'_'$classB'.csv' --fileType $dataType \
+    --num_labels 2 --ckptFolderAndFile $BASE_FOLDER/ckptDir/$dataType'_sept_nclasses_2_v20_'$classA'_'$classB'.pth' --classSplit $classPairSrting --gpu_id 0
+
+
+
+
+    classPairSrting='1,3'
+    classA='1'
+    classB='3'
+    python $BASE_FOLDER/test_2class.py --test_data $BASE_FOLDER/data/$dataType'_test_data_'$classA'_'$classB'.csv' --test_label $BASE_FOLDER/data/$dataType'_test_label_'$classA'_'$classB'.csv' --fileType $dataType \
+    --num_labels 2 --ckptFolderAndFile $BASE_FOLDER/ckptDir/$dataType'_sept_nclasses_2_v20_'$classA'_'$classB'.pth' --classSplit $classPairSrting --gpu_id 0
+
+
+
+    classPairSrting='2,3'
+    classA='2'
+    classB='3'
+    python $BASE_FOLDER/test_2class.py --test_data $BASE_FOLDER/data/$dataType'_test_data_'$classA'_'$classB'.csv' --test_label $BASE_FOLDER/data/$dataType'_test_label_'$classA'_'$classB'.csv' --fileType $dataType \
+    --num_labels 2 --ckptFolderAndFile $BASE_FOLDER/ckptDir/$dataType'_sept_nclasses_2_v20_'$classA'_'$classB'.pth' --classSplit $classPairSrting --gpu_id 0
 
 fi
